07a7cb36d7fd0fe0ad549ead760c396ff172adc3
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-02-06 00:28:20 -0500

    Add logic to get column information.

--- local/project.go
+++ local/project.go
@@ -1,7 +1,14 @@
 package local
 
 import (
+	"context"
+	"log"
 	"sync"
+	"time"
+
+	"github.com/stephen-soltesz/github-webhook-poc/events/projects"
+
+	"github.com/stephen-soltesz/github-webhook-poc/githubx"
 
 	"github.com/google/go-github/github"
 	"github.com/stephen-soltesz/pretty"
@@ -13,6 +20,23 @@ var mux = sync.Mutex{}
 func ProjectCardEvent(event *github.ProjectCardEvent) error {
 	mux.Lock()
 	defer mux.Unlock()
+	switch event.GetAction() {
+	case "moved":
+		event.GetProjectCard().GetColumnID()
+		client := githubx.NewClient(event.GetInstallation().GetID())
+		if client == nil {
+			return ErrNewClient
+		}
+		ev := projects.NewEvent(client, event)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+		col := ev.GetColumn(ctx)
+		if col != nil {
+			pretty.Print(col)
+		}
+		// ev := issues.NewEvent(c.getIface(client), event)
+	}
+	log.Print("project card event:")
 	pretty.Print(event)
 	return nil
 }
@@ -21,6 +45,7 @@ func ProjectCardEvent(event *github.ProjectCardEvent) error {
 func ProjectColumnEvent(event *github.ProjectColumnEvent) error {
 	mux.Lock()
 	defer mux.Unlock()
+	log.Print("project column event:")
 	pretty.Print(event)
 	return nil
 }
@@ -29,6 +54,7 @@ func ProjectColumnEvent(event *github.ProjectColumnEvent) error {
 func ProjectEvent(event *github.ProjectEvent) error {
 	mux.Lock()
 	defer mux.Unlock()
+	log.Print("project event:")
 	pretty.Print(event)
 	return nil
 }
