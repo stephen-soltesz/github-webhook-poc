6599db95a2956e467e766feb9e48aa3cc2386c16
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-02-18 18:52:15 -0500

    Add options for indexing and filtering

--- slice/slice.go
+++ slice/slice.go
@@ -9,3 +9,25 @@ func ContainsString(slice []string, value string) bool {
 	}
 	return false
 }
+
+// IndexString returns the index of value in slice, or -1 if value is not present.
+func IndexString(slice []string, value string) int {
+	for i := range slice {
+		if slice[i] == value {
+			return i
+		}
+	}
+	return -1
+}
+
+func FilterStrings(slice []string, values []string) []string {
+	result := []string{}
+	for _, current := range slice {
+		if ContainsString(values, current) {
+			// Skip this value since we want it removed.
+			continue
+		}
+		result = append(result, current)
+	}
+	return result
+}
